d444b3ecbd34a9069a39e18503370ce4ed14b113
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-13 18:25:36 -0300

    Script to add more hour (page give classes)

---
+++ Maik/scripts/addField.js
@@ -0,0 +1,21 @@
+// Procurar o botão / Quando clicar no botão
+document.querySelector("#add-time").addEventListener('click', cloneField);
+
+// Executar uma ação
+function cloneField() {
+    // Duplicar os campos
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    
+    // Limpar os campos
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    fields.forEach(function(field) {
+        field.value = "";
+    });
+
+
+    // Colocar na página 
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
+
